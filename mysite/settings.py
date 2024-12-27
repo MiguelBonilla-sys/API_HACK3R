468cfe0cc7084eca7f026a7fb585887e7dfc12be
MiguelBonilla-sys <mabonillat@academia.usbbog.edu.co>
2024-12-27 17:45:39 -0500

    settings.py: add ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
 
