b9f95e846d4371f7afbbbc82f40cd24733340dfe
MiguelBonilla-sys <mabonillat@academia.usbbog.edu.co>
2024-12-27 20:46:46 -0500

    Agregar configuración para almacenamiento en la nube con Cloudinary y actualizar dependencias

--- mysite/settings.py
+++ mysite/settings.py
@@ -171,3 +171,12 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': 3600.0,  # Ejecutar cada hora
     },
 }
+
+# Cloud storage settings
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
