3b90477d8e4a130bee5a54721c3f353d32fa518a
MiguelBonilla-sys <mabonillat@academia.usbbog.edu.co>
2024-12-27 20:52:48 -0500

    Agregar campo de imagen al serializador de noticias

--- blog/Serializers/NoticiasSerializer.py
+++ blog/Serializers/NoticiasSerializer.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from blog.Models.NoticiasModel import Noticias
-# Convierte el modelo Noticias(Python) en un JSON para ser
-# Consumido por la API
+
 class NoticiasSerializer(serializers.ModelSerializer):
+    imagen_noticia = serializers.ImageField()
+
     class Meta:
         model = Noticias
         fields = '__all__'
